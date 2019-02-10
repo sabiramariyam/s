81e5de45bc9b4c87622ca76e912a3fe924ed737a
sabiramariyam <42670623+sabiramariyam@users.noreply.github.com>
2019-02-10 20:40:44 +0530

    Add files via upload

---
+++ cost.py
@@ -0,0 +1,27 @@
+a=list(input())
+
+b=list(input())
+
+d=len(a)
+
+e=len(b)
+
+i=0
+
+j=0
+
+c=[]
+
+while d>0:
+    
+    if a[i]==b[j]:
+        
+        c.append(a[i])
+    
+    j=j+1
+    
+    i=i+1
+    
+    d=d-1
+
+print(e-len(c))
