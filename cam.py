5d7a9f8fd952c046177c2dcb32264606cae618d5
sabiramariyam <42670623+sabiramariyam@users.noreply.github.com>
2019-02-10 14:02:05 +0530

    Add files via upload

---
+++ cam.py
@@ -0,0 +1,8 @@
+input =str(input("")).lower() 
+words = input.split(' ')
+capitalized = []
+for word in words:
+    final_words = word[0].upper() + word[1:]
+    capitalized.append(final_words)
+output = ' '.join(capitalized)
+print (output) 
